5cb9df21e88c3f7ecb10103ab00dece3c1827611
korolev5526 <korolev5526@yandex.ru>
2021-11-06 22:09:53 +0700

    added code for deny empty login field lesson 1_additionally

--- js/main.js
+++ js/main.js
@@ -5,51 +5,51 @@ const closeAuth = document.querySelector(".close-auth");
 const loginForm = document.getElementById("logInForm");
 const inputLogin = document.getElementById("login");
 const inputPassword = document.getElementById("password");
-const userName = document.querySelector('.user-name');
+const userName = document.querySelector(".user-name");
 
 const login = (user) => {
-    buttonAuth.style.display = 'none';
+  buttonAuth.style.display = "none";
 
-    buttonAuthOut.style.display = 'flex';
-    userName.textContent = user.login;
-    userName.style.display = 'flex';
-    modalAuth.style.display = 'none';
-}
+  buttonAuthOut.style.display = "flex";
+  userName.textContent = user.login;
+  userName.style.display = "flex";
+  modalAuth.style.display = "none";
+};
 
 const logout = () => {
-    buttonAuth.style.display = 'flex';
+  buttonAuth.style.display = "flex";
 
-    buttonAuthOut.style.display = 'none';
-    userName.textContent = "";
-    userName.style.display = 'none';
-    localStorage.removeItem('user');
-}
+  buttonAuthOut.style.display = "none";
+  userName.textContent = "";
+  userName.style.display = "none";
+  localStorage.removeItem("user");
+};
 
 buttonAuth.addEventListener("click", () => {
-    modalAuth.style.display = 'flex';
-})
+  modalAuth.style.display = "flex";
+});
 
 closeAuth.addEventListener("click", () => {
-    modalAuth.style.display = 'none';
-})
+  modalAuth.style.display = "none";
+});
 
-buttonAuthOut.addEventListener('click', () => {
-    logout();
-})
+buttonAuthOut.addEventListener("click", () => {
+  logout();
+});
 
-loginForm.addEventListener('submit', (event) => {
-    event.preventDefault();
-
+loginForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  if (inputLogin.value != "") {
     const user = {
-        login: inputLogin.value,
-        password: inputPassword.value,
-    }
+      login: inputLogin.value,
+      password: inputPassword.value,
+    };
 
-
-    localStorage.setItem('user', JSON.stringify(user));
+    localStorage.setItem("user", JSON.stringify(user));
     login(user);
-})
+  } else alert("Заполните поле ЛОГИН!!!");
+});
 
-if (localStorage.getItem('user')) {
-    login(JSON.parse(localStorage.getItem('user')));
+if (localStorage.getItem("user")) {
+  login(JSON.parse(localStorage.getItem("user")));
 }
