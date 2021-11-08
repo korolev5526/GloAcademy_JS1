b091c1e45501090a5ff8d80a616ae55fba5177e3
korolev5526 <korolev5526@yandex.ru>
2021-11-08 22:53:07 +0700

    add dynamic code for restaurant.html edit fetch for every restaurant add data attribute to localStorage

--- js/menu.js
+++ js/menu.js
@@ -1,14 +1,42 @@
-const restourant = "food-band";
+const cardsMenu = document.querySelector(".cards-menu");
 
 const renderItems = (data) => {
-  console.log(data);
+  data.forEach(({ description, id, image, name, price }) => {
+    const card = document.createElement("div");
+    card.classList.add("card");
+
+    card.innerHTML = `
+            <img src="${image}" alt="${name}" class="card-image" />
+            <div class="card-text">
+              <div class="card-heading">
+                <h3 class="card-title card-title-reg">${name}</h3>
+              </div>
+              <div class="card-info">
+                <div class="ingredients">${description}</div>
+              </div>
+              <div class="card-buttons">
+                <button class="button button-primary button-add-cart">
+                  <span class="button-card-text">В корзину</span>
+                  <span class="button-cart-svg"></span>
+                </button>
+                <strong class="card-price-bold">${price} ₽</strong>
+              </div>
+            </div>
+    `;
+
+    cardsMenu.append(card);
+  });
 };
 
-fetch(`./db/${restourant}.json`)
-  .then((response) => response.json())
-  .then((data) => {
-    renderItems(data);
-  })
-  .catch((error) => {
-    console.log(error);
-  });
+if (localStorage.getItem("restaurant")) {
+  fetch(`./db/${localStorage.getItem("restaurant")}`)
+    .then((response) => response.json())
+    .then((data) => {
+      renderItems(data);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+} else {
+  window.location.href = "/";
+}
