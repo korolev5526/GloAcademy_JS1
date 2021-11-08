1f6438db4d3a9d50efcd2eebad74000f8972c1b1
korolev5526 <korolev5526@yandex.ru>
2021-11-08 21:18:40 +0700

    reconfigure structure of project for webpack

---
+++ src/modules/partners.js
@@ -0,0 +1,16 @@
+const partners = () => {
+  const renderItems = (data) => {
+    console.log(data);
+  };
+
+  fetch("https://glo-academy-js1-default-rtdb.firebaseio.com/db/partners.json")
+    .then((response) => response.json())
+    .then((data) => {
+      renderItems(data);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
+export default partners;
