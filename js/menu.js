cff6fa9524c7cb9e520a4ddaa1353ae6ab2ec599
korolev5526 <korolev5526@yandex.ru>
2021-11-09 11:50:01 +0700

    fixed bug on cart

--- js/menu.js
+++ js/menu.js
@@ -1,16 +1,16 @@
 const menu = () => {
   const cardsMenu = document.querySelector(".cards-menu");
 
-  const cartArray = localStorage.getItem("cart")
-    ? JSON.parse(localStorage.getItem("cart"))
-    : [];
-
   const changeTitle = (restaurant) => {
     const restaurantTitle = document.querySelector(".restaurant-title");
     restaurantTitle.textContent = restaurant.name;
   };
 
   const addToCart = (cartItem) => {
+    const cartArray = localStorage.getItem("cart")
+      ? JSON.parse(localStorage.getItem("cart"))
+      : [];
+
     if (cartArray.some((item) => item.id === cartItem.id)) {
       cartArray.map((item) => {
         if (item.id === cartItem.id) {
